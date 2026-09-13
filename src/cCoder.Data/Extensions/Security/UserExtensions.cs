03eb357c06146be099977cd75d7a3caed4f08104
TehWardy <6891030+TehWardy@users.noreply.github.com>
2026-09-13 11:30:07 +0100

    Remove same-layer security extension dependencies

--- src/cCoder.Data/Extensions/Security/UserExtensions.cs
+++ src/cCoder.Data/Extensions/Security/UserExtensions.cs
@@ -10,14 +10,22 @@ public static class UserExtensions
         appId.HasValue
         && (user?.Roles?.Any(predicate: role =>
             role.Role?.AppId == appId.Value
-            && role.Role.Allows(user: user, privilege: "app_admin")) ?? false);
+            && user.Roles.Any(predicate: userRole =>
+                userRole.RoleId == role.Role.Id)
+            && role.Role.Privileges.Any(predicate: privilege =>
+                privilege == "app_admin")) ?? false);
 
     public static bool Can(this User user, int? appId, string operation)
     {
         string normalizedOperation = operation?.ToLowerInvariant() ?? string.Empty;
 
         return user != null
-            && ((appId.HasValue && user.IsAdminOfApp(appId: appId.Value))
+            && ((appId.HasValue && user.Roles?.Any(predicate: role =>
+                role.Role?.AppId == appId.Value
+                && user.Roles.Any(predicate: userRole =>
+                    userRole.RoleId == role.Role.Id)
+                && role.Role.Privileges.Any(predicate: privilege =>
+                    privilege == "app_admin")) == true)
                 || (user.Roles?.Any(predicate: role =>
                     (!appId.HasValue || role.Role?.AppId == appId.Value)
                     && (role.Role?.Privileges?.Contains(item: normalizedOperation) ?? false)) ?? false));
